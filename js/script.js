623b42c74d7638fb03338f2f1c9db267c8a6c355
consins <consins@cubee.com>
2017-10-19 16:41:10 +0800

    提交修改密码

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;  
     var pwd=document.getElementById("pwd").value;
-    if(userName=="小郭郭" &&  pwd=="20170901"){ 
+    if(userName=="小郭郭" &&  pwd=="19920903"){ 
 		event.preventDefault();
 		$('form').fadeOut(500);
 		$('.wrapper').addClass('form-success');
 		setTimeout(function(){location.href="BirthdayCake.html";},2000);
 	}
 	else{
-		alert("嘿嘿不正确,密码是:20170607");
+		alert("嘿嘿不正确,密码是:19920903");
 	}
 });
